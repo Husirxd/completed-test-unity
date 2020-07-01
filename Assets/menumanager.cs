7dcd39e88b02d77dc9ccecf83c5ecd38c6933730
Husirxd <husirxd@gmail.com>
2020-07-01 17:50:57 +0200

    v1

---
+++ Assets/menumanager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class menumanager : MonoBehaviour
+{
+   public GameObject Popup,Itext;
+
+   public AudioSource Myaudio;
+   float time;
+
+
+//popupmanaganent
+public void openPopup(){
+Popup.SetActive(true);
+
+}
+
+public void closePopup(){
+  Popup.SetActive(false); 
+}
+
+    
+
+//limitedtime managament
+public void openInterval(){
+
+time=3f;
+//Istnieje możliwość użycia Coroutine do osiągniecią efektu. Zdecydowałem się na Update ze względu na chęć wyświetlenia pozostałego czasu
+//Itext.SetActive(true);
+//StartCoroutine(Timer());
+} 
+/*
+ IEnumerator Timer(){
+yield return new WaitForSeconds(3f);
+Itext.SetActive(false);
+}
+*/
+
+void Update(){
+if(time>0){
+time -=Time.deltaTime;
+Itext.SetActive(true);
+Itext.GetComponent<TextMeshProUGUI>().text = "I will disappear in "+Mathf.Ceil(time);
+}else{
+    Itext.SetActive(false);
+}
+
+}
+
+
+//audio managament
+public void playAudio(){
+Myaudio.Play();
+
+}
+
+}
